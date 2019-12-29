cacbdefc8263308d9154f40e88a34eb9cc86987f
yniss <39021999+yniss@users.noreply.github.com>
2019-12-29 22:25:55 +0200

    Add files via upload

--- raa_logger.py
+++ raa_logger.py
@@ -1,18 +1,83 @@
 import logging
+import queue
+import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
+from tkinter import N, S, E, W
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG) #TODO: temp for debug
+logger_name = logger.name #TODO: pass to thread process, so he can call  "logger = logging.getLogger(logger_name)"
+print(f"XXX - logger.logger:{logger}") #TODO: debug
+
+##  #TODO: figure out a way - where to add a logger file handler for all messages (including debug level)
+##  # Create file handler
+##  f_handler = logging.FileHandler(file_name='raa.log', mode='w')
+##  f_handler.setLevel(logging.DEBUG)
+##  
+##  # Create formatter and add it to handler
+##  f_format = logging.Formatter('%(asctime)s %(name)s [%(levelname)s]: %(message)s')
+##  f_handler.setFormatter(f_format)
+##  
+##  # Add handler to the logger
+##  logger.addHandler(f_handler)
+
 
 class QueueHandler(logging.Handler):
     """Class to send logging records to a queue
-    
     It can be used from different threads
+    The ConsoleUi class polls this queue to display records in a ScrolledText widget
     """
+    # Example from Moshe Kaplan: https://gist.github.com/moshekaplan/c425f861de7bbf28ef06
+    # (https://stackoverflow.com/questions/13318742/python-logging-to-tkinter-text-widget) is not thread safe!
+    # See https://stackoverflow.com/questions/43909849/tkinter-python-crashes-on-new-thread-trying-to-log-on-main-thread
+
     def __init__(self, log_queue):
         super().__init__()
         self.log_queue = log_queue
-        
-        def emit(self, record):
-            self.log_queue.put(record)
+
+    def emit(self, record):
+        self.log_queue.put(record)
 
 
+class ConsoleUi:
+    """Poll messages from a logging queue and display them in a scrolled text widget"""
 
-logger = logging.getLogger(__name__)
-print(f"XXX - my_log_queue.logger:{logger}")
+    def __init__(self, frame):
+        self.frame = frame
+        # Create a ScrolledText wdiget
+        self.scrolled_text = ScrolledText(frame, state='disabled', height=12)
+        self.scrolled_text.grid(row=0, column=0, sticky=(N, S, W, E)) #TODO: should change grid row/col?
+        self.scrolled_text.configure(font='TkFixedFont')
+        self.scrolled_text.tag_config('INFO', foreground='black')
+        self.scrolled_text.tag_config('DEBUG', foreground='gray')
+        self.scrolled_text.tag_config('WARNING', foreground='orange')
+        self.scrolled_text.tag_config('ERROR', foreground='red')
+        self.scrolled_text.tag_config('CRITICAL', foreground='red', underline=1)
+        # Create a logging handler using a queue
+        self.log_queue = queue.Queue()
+        self.queue_handler = QueueHandler(self.log_queue)
+        formatter = logging.Formatter('%(message)s')
+        self.queue_handler.setFormatter(formatter)
+        logger.addHandler(self.queue_handler)
+        # Start polling messages from the queue
+        self.frame.after(100, self.poll_log_queue)
+
+    def display(self, record):
+        msg = self.queue_handler.format(record)
+        self.scrolled_text.configure(state='normal')
+        self.scrolled_text.insert(tk.END, msg + '\n', record.levelname)
+        self.scrolled_text.configure(state='disabled')
+        # Autoscroll to the bottom
+        self.scrolled_text.yview(tk.END)
+
+    def poll_log_queue(self):
+        # Check every 100ms if there is a new message in the queue to display
+        while True:
+            try:
+                record = self.log_queue.get(block=False)
+            except queue.Empty:
+                break
+            else:
+                self.display(record)
+        self.frame.after(100, self.poll_log_queue)
+
